267c4d8d356fd5a99303d0571eed58bb95e53d1d
hermannm <hermann.morkrid@gmail.com>
2021-12-15 17:58:11 +0100

    improve docs and export defaults

--- query_options.go
+++ query_options.go
@@ -8,31 +8,35 @@ import (
 // Options to customize the public IP querying with your own list of API URLs,
 // or your own timeout limit.
 type QueryOptions struct {
-	APIs    []string
+	// URLs to API endpoints that return your public IP as a simple string.
+	// See defaultAPIs for examples.
+	APIs []string
+
+	// Time in milliseconds before the API calls should time out.
 	Timeout int
 }
 
 // The default list of APIs to query to find your public IP.
 // These have almost guaranteed uptime, and essentially no usage limit.
-var defaultAPIs = []string{
-	"https://api.ipify.org/",
-	"https://ip.seeip.org/",
-	"https://myexternalip.com/raw", // Be aware: this API will block you for >30 calls/minute, do not abuse
+var DefaultAPIs = []string{
+	"https://api.ipify.org/",       // No usage limit.
+	"https://ip.seeip.org/",        // No usage limit.
+	"https://myexternalip.com/raw", // This API will block you for >30 requests/minute, do not abuse.
 }
 
-// Default number of milliseconds to wait for API responses before timing out.
-const defaultTimeout int = 2000
+// Default time in milliseconds to wait for API responses before timing out.
+const DefaultTimeout int = 2000
 
 // Checks for the validity of provided query options.
 // Returns error if a provided option was invalid.
 // If not all query options are provided, injects the default options for those.
 func (opts *QueryOptions) validate() error {
 	if opts.Timeout == 0 {
-		opts.Timeout = defaultTimeout
+		opts.Timeout = DefaultTimeout
 	}
 
 	if len(opts.APIs) == 0 {
-		opts.APIs = defaultAPIs
+		opts.APIs = DefaultAPIs
 
 		// Assumes that default APIs work correctly, and so does not validate further.
 		return nil
